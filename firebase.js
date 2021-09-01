e3491181d52a10624c0fd1f390d5b917be313327
scott collier <scott.h.collier@gmail.com>
2021-09-01 14:19:56 +0100

    scss structure added and react template files removed

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDfhjgC3nIMlqZkKDQorZsBrvD4Vfi7f_s",
+    authDomain: "clone-6a6b4.firebaseapp.com",
+    projectId: "clone-6a6b4",
+    storageBucket: "clone-6a6b4.appspot.com",
+    messagingSenderId: "470394145898",
+    appId: "1:470394145898:web:51cc51387056db6d94ce13",
+    measurementId: "G-BQ000JLJZF",
+};
